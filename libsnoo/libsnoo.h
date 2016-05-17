d301514fe1c0203316d4849badc223d5452f2de7
William Lee <wlee@mochify.com>
2016-05-17 00:41:37 -0400

    Initial Commit

---
+++ libsnoo/libsnoo.h
@@ -0,0 +1,19 @@
+//
+//  libsnoo.h
+//  libsnoo
+//
+//  Created by William Lee on 5/17/16.
+//  Copyright © 2016 functots. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for libsnoo.
+FOUNDATION_EXPORT double libsnooVersionNumber;
+
+//! Project version string for libsnoo.
+FOUNDATION_EXPORT const unsigned char libsnooVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <libsnoo/PublicHeader.h>
+
+
